1ece0b7a33975dfae410a62651ae619dad9a0cc0
Andreas Hofheinz <andreas.hofheinz@outlook.com>
2020-06-07 19:47:11 +0200

    Added data for second map level for map germany

--- inst/app_germany.R
+++ inst/app_germany.R
@@ -3,6 +3,26 @@ library(leaflet)
 library(leafdown)
 library(dplyr)
 library(readr)
+library(stringr)
+
+# Load data and spdfs ----------------------------------------------------------
+ger1 <- readRDS("extdata/gadm36_DEU_1_sp.rds")
+ger2 <- readRDS("extdata/gadm36_DEU_2_sp.rds")
+# Correct Umlaute
+ger2@data[c(76, 99, 136, 226), "NAME_2"] <- c(
+  "Fürth (Kreisfreie Stadt)",
+  "München (Kreisfreie Stadt)",
+  "Osnabrück (Kreisfreie Stadt)",
+  "Würzburg (Kreisfreie Stadt)"
+)
+spdfs_list <- list(ger1, ger2)
+gpd_states <- read.csv2("extdata/gdp2014_germany_1.csv", fileEncoding = "UTF-8",
+                        stringsAsFactors = FALSE)
+gpd_admin_districts <- read.csv2("extdata/gdp2014_germany_2.csv", fileEncoding = "UTF-8",
+                        stringsAsFactors = FALSE)
+gpd_admin_districts$Admin_District <- str_replace(gpd_admin_districts$Admin_District,
+                                                  ", Stadt", "")
+
 
 # Define UI for leafdown app
 ui <- shiny::fluidPage(
@@ -19,13 +39,6 @@ ui <- shiny::fluidPage(
 
 # Define server for leafdown app
 server <- function(input, output) {
-  ger1 <- readRDS("extdata/gadm36_DEU_1_sp.rds")
-  ger2 <- readRDS("extdata/gadm36_DEU_2_sp.rds")
-  gpd_states <- read.csv2("extdata/gdp2014_germany_1.csv",
-                           fileEncoding = "UTF-8", stringsAsFactors = FALSE)
-
-
-  spdfs_list <- list(ger1, ger2)
   my_leafdown <- Leafdown$new(spdfs_list, "leafdown", input)
 
   rv <- reactiveValues()
@@ -46,18 +59,58 @@ server <- function(input, output) {
     rv$update_leafdown
     data <- my_leafdown$get_current_data()
     if (my_leafdown$curr_map_level == 1) {
-      data <- left_join(data, gpd_states, by = c("NAME_1" = "Federal_State"))
+      data <- data %>% left_join(gpd_states, by = c("NAME_1" = "Federal_State"))
     } else {
-      data$GDP_2014 <- 1:nrow(data)
+      data <- data %>% left_join(gpd_admin_districts, by = c("NAME_2" = "Admin_District"))
     }
-
     my_leafdown$add_data(data)
-
-    my_leafdown$draw_leafdown(
-      fillColor = ~leaflet::colorNumeric("Greens", GDP_2014)(GDP_2014),
+    map <- my_leafdown$draw_leafdown(
+      fillColor = ~ leaflet::colorNumeric("Greens", GDP_2014)(GDP_2014),
       weight = 2, fillOpacity = 0.7, color = "green"
     )
+    map <- map %>%
+      addLegend("bottomright", pal = leaflet::colorNumeric("Greens", data$GDP_2014),
+                values = ~GDP_2014, title = "GDP (2014)",
+                labFormat = labelFormat(prefix = "€"), opacity = 1)
   })
 }
 
 shinyApp(ui, server)
+
+
+
+# library(readxl)
+# gdp_admin_districts <- read_excel("inst/extdata/BIP_Landkreise.xlsx")
+# gdp_admin_districts <- gdp_admin_districts[,c("name", "BIP_14")]
+# head(gdp_admin_districts)
+# ger2 <- readRDS("inst/extdata/gadm36_DEU_2_sp.rds")
+# # Correct missing Umlaute
+# ger2@data[c(76, 99, 136, 226), "NAME_2"] <- c(
+#   "Fürth (Kreisfreie Stadt)",
+#   "München (Kreisfreie Stadt)",
+#   "Osnabrück (Kreisfreie Stadt)",
+#   "Würzburg (Kreisfreie Stadt)"
+# )
+# head(ger2@data)
+#
+# library(stringr)
+#
+# gdp_admin_districts2 <- gdp_admin_districts
+# gdp_admin_districts2$name <- gdp_admin_districts2$name %>% str_replace(", Stadt", "")
+#
+# data <- inner_join(ger2@data, gdp_admin_districts2, by = c("NAME_2" = "name"))
+#
+# data2 <- anti_join(ger2@data, gdp_admin_districts2, by = c("NAME_2" = "name"))
+#
+# data2$NAME_2
+#
+# gdp_admin_districts2 <- as.data.frame(gdp_admin_districts)
+# names(gdp_admin_districts2) <- c("Admin_District", "GDP_2014")
+#
+# write.csv2(gdp_admin_districts2, file = "inst/extdata/gdp2014_germany_2.csv",
+#            fileEncoding = "UTF-8", row.names = FALSE)
+#
+# gdp_admin_districts3 <- read.csv2("inst/extdata/gdp2014_germany_2.csv",
+#                                  fileEncoding = "UTF-8", stringsAsFactors = FALSE)
+#
+# gdp_admin_districts3
