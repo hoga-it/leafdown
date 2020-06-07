745752cd72058cfa735d290ef4b64035d9410b6a
Andreas Hofheinz <andreas.hofheinz@outlook.com>
2020-06-07 17:53:58 +0200

    Added data for first map level for german app

--- inst/app_germany.R
+++ inst/app_germany.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(leaflet)
 library(leafdown)
+library(dplyr)
+library(readr)
 
 # Define UI for leafdown app
 ui <- shiny::fluidPage(
@@ -17,9 +19,10 @@ ui <- shiny::fluidPage(
 
 # Define server for leafdown app
 server <- function(input, output) {
-  ger1 <- readRDS("inst/extdata/gadm36_DEU_1_sp.rds")
-  ger2 <- readRDS("inst/extdata/gadm36_DEU_2_sp.rds")
-
+  ger1 <- readRDS("extdata/gadm36_DEU_1_sp.rds")
+  ger2 <- readRDS("extdata/gadm36_DEU_2_sp.rds")
+  gpd_states <- read.csv2("extdata/gdp2014_germany_1.csv",
+                           fileEncoding = "UTF-8", stringsAsFactors = FALSE)
 
 
   spdfs_list <- list(ger1, ger2)
@@ -38,16 +41,20 @@ server <- function(input, output) {
     rv$update_leafdown <- rv$update_leafdown + 1
   })
 
-
   output$leafdown <- renderLeaflet({
     req(spdfs_list)
     rv$update_leafdown
     data <- my_leafdown$get_current_data()
-    data$y <- 1:nrow(data)
+    if (my_leafdown$curr_map_level == 1) {
+      data <- left_join(data, gpd_states, by = c("NAME_1" = "Federal_State"))
+    } else {
+      data$GDP_2014 <- 1:nrow(data)
+    }
+
     my_leafdown$add_data(data)
 
     my_leafdown$draw_leafdown(
-      fillColor = ~leaflet::colorNumeric("Greens", y)(y),
+      fillColor = ~leaflet::colorNumeric("Greens", GDP_2014)(GDP_2014),
       weight = 2, fillOpacity = 0.7, color = "green"
     )
   })
