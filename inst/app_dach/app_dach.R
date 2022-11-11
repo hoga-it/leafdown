51a87168a5f75bd6688f9f8f4d0eb5564fd15d9b
Andreas <andreas.hofheinz@outlook.com>
2022-11-11 19:02:34 +0100

    #42 join on current all parent levels

--- inst/app_dach/app_dach.R
+++ inst/app_dach/app_dach.R
@@ -44,16 +44,12 @@ data_sim_y_level_3$level <- 3
 data_sim_y_level_2$level <- 2
 data_sim_y_level_1$level <- 1
 
-data_sim_y_level_3$area <- data_sim_y_level_3$NAME_2
-data_sim_y_level_2$area <- data_sim_y_level_2$NAME_1
-data_sim_y_level_1$area <- data_sim_y_level_1$NAME_0
-
-data_sim_y <- rbind(
-  data_sim_y_level_3[, c("area", "NAME_0", "y", "level")],
-  data_sim_y_level_2[, c("area", "NAME_0", "y", "level")],
-  data_sim_y_level_1[, c("area", "NAME_0", "y", "level")]
+data_sim_y <- bind_rows(
+  data_sim_y_level_3,
+  data_sim_y_level_2,
+  data_sim_y_level_1
 )
-names(data_sim_y) <- c("area", "scnd_join", "y", "level")
+
 data_sim_y$y <- round(data_sim_y$y, 0)
 
 create_labels <- function(data, map_level) {
@@ -114,17 +110,8 @@ server <- function(input, output) {
     meta_data <- my_leafdown$curr_data
     curr_map_level <- my_leafdown$curr_map_level
     data_curr_map_level <- data_sim_y[data_sim_y$level == curr_map_level, ]
-
-    # on level 3 there are multiple areas with the same name, so we have to use
-    # a combination of name and GID to make them unique
-    if(curr_map_level == 3) {
-      join_col_lhs <- paste0("NAME_", curr_map_level - 1)
-      data <- meta_data %>%
-        left_join(data_curr_map_level, by = c("NAME_2" = "area", "NAME_0" = "scnd_join"))
-    } else {
-      join_col_lhs <- paste0("NAME_", curr_map_level - 1)
-      data <- meta_data %>% left_join(data_curr_map_level, by = setNames("area", join_col_lhs))
-    }
+    join_col_lhs <- paste0("NAME_", seq_len(curr_map_level) - 1)
+    data <- meta_data %>% left_join(data_curr_map_level, by = join_col_lhs, suffix = c("", ".y"))
 
     # add the data back to the leafdown object
     my_leafdown$add_data(data)
